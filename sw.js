fd00f4b448e6db4e690ef3115da2e201e4dbb8cf
tatendafreddy-lab <tatendafreddy@gmail.com>
2026-04-03 17:17:35 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Minimal: no caching to keep behavior simple for now
