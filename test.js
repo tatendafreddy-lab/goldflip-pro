48c8459991e142db7942f4a9c522853e8e2d6c74
tatendafreddy-lab <tatendafreddy@gmail.com>
2026-04-04 15:44:00 +0200

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    status: "ok",
-    hasKey: !!process.env.VITE_GOLD_API_KEY,
-    keyPreview: process.env.VITE_GOLD_API_KEY
-      ? `${process.env.VITE_GOLD_API_KEY.slice(0, 8)}...`
-      : null,
-  });
-}
